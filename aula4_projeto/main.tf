2ff10c8d207c75115f40e176191b5c5f1457fa76
paulo yun <paulo.yun@gmail.com>
2025-11-15 11:19:05 -0300

    fix

--- aula4_projeto/main.tf
+++ aula4_projeto/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = ">= 1.0"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -19,7 +19,9 @@ data "digitalocean_ssh_key" "default" {
 module "do_wp_stack" {
   source = "./modules/do-wp-stack"
 
-  do_token    = var.do_token
-  region      = var.region
-  vm_wp_count = var.vm_wp_count
+  do_token     = var.do_token
+  region       = var.region
+  vm_wp_count  = var.vm_wp_count
+  vm_wp_image  = "ubuntu-22-04-x64"
+  vm_wp_size   = "s-2vcpu-2gb"
 }
